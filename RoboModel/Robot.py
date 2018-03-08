7b0dc5b44cc70f00b9ff66a969fb79ce99670eff
norik16 <ron.norik@gmail.com>
2018-03-09 00:04:38 +0100

    Testing sensors with motors

--- RoboModel/Robot.py
+++ RoboModel/Robot.py
@@ -43,7 +43,7 @@ class Robot:
         # self.L_R_RATIO = 6.9 / 5
         self.L_R_RATIO = 1.3076923076923077
         self.ACCELERATION = 10
-        self.MOTORS_MIN_SPEED = 20
+        self.MOTORS_MIN_SPEED = 16
         self.ACC_DELAY = 0.005
 
         # Other
