45f183d7db7ab5f07bbb3128196aefad682f5188
norik16 <ron.norik@gmail.com>
2018-03-08 15:20:17 +0100

    Testing sensors with motors

--- RoboModel/Robot.py
+++ RoboModel/Robot.py
@@ -40,9 +40,9 @@ class Robot:
         print('Init complete!')
 
         self.start_listening()
-        while 1:
-            self.blow_fans()
-            sleep(3)
+        # while 1:
+        #     self.blow_fans()
+        #     sleep(3)
         self.blow_fans()
         # self.go_test(0, 2)
         self.main_cycle()
